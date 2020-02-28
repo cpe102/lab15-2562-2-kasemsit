1bceab83a9e0c91235cd212a4055ee8dd503f2de
Kasemsit Teeyapan <kasemsit@gmail.com>
2020-02-28 17:13:31 +0700

    done

--- lab15_1.cpp
+++ lab15_1.cpp
@@ -1,4 +1,5 @@
 #include<iostream>
+#include<iomanip>
 #include<cstdlib>
 #include<ctime>
 using namespace std;
@@ -12,7 +13,8 @@ void showData(double *,int,int);
 int main(){
 	srand(time(0));
 	const int N = 7, M = 7;
-	double data[N][M] = {};
+	//double data[N][M] = {};
+	double data[N][M] = {0.58, 0.07, 0.72, 0.28, 0.12, 0.99, 0.01, 0.33, 0.42, 0.36, 0.08, 0.25, 0.78, 0.08, 0.4, 0.63, 0.73, 0.09, 0.0, 0.11, 0.17, 0.47, 0.97, 0.3, 0.3, 0.78, 0.31, 0.69, 0.36, 0.71, 0.12, 0.6, 0.79, 0.5, 0.89, 0.91, 0.48, 0.56, 0.9, 0.56, 0.92, 0.98, 0.47, 0.7, 0.73, 0.87, 0.99, 0.45, 0.96};
 	double result[N] = {};
 	double *dPtr = data[0];
 	randData(dPtr,N,M);
@@ -25,3 +27,29 @@ int main(){
 }
 
 //Write definition of randData(), showData() and findRowSum()
+void randData(double *data,int N,int M) {
+	
+	return;
+}
+
+void findRowSum(const double *data,double *output,int N,int M) {
+	
+	for (int i=0; i<N; i++) {		
+		for (int j=0; j<M; j++) {
+			*(output+i) += *(data + i*M+j);						
+		}   		
+	}
+	return;
+}
+
+void showData(double *data,int N,int M) {
+	
+	for (int i=0; i<N; i++) {		
+		for (int j=0; j<M; j++) {
+			cout << setw(5) << *(data+ i*M+j) << " ";				 
+		}   
+		cout << '\n';
+	}
+	
+	return;
+}
--- lab15_2.cpp
+++ lab15_2.cpp
@@ -1,4 +1,5 @@
 #include<iostream>
+#include<iomanip>
 #include<cstdlib>
 #include<ctime>
 using namespace std;
@@ -12,7 +13,9 @@ void showData(double *,int,int);
 int main(){
 	srand(time(0));
 	const int N = 7, M = 7;
-	double data[N][M] = {};
+	//double data[N][M] = {};
+	
+	double data[N][M] = {0.38, 0.34, 0.36, 0.17, 0.5, 0.88, 0.71, 0.08, 0.93, 0.85, 0.64, 0.72, 0.31, 0.15, 0.01, 0.86, 0.09, 0.14, 0.22, 0.85, 0.09,0.13, 0.34, 0.42, 0.09, 0.8, 0.77, 0.14, 0.71, 0.55, 0.64, 0.75, 0.55, 1.0, 0.92, 0.04, 0.88, 0.62, 0.12, 0.8, 0.13, 0.77, 0.18, 0.44, 0.58, 0.19, 0.29, 0.67, 0.33};
 	double result[M] = {};
 	double *dPtr = data[0];
 	randData(dPtr,N,M);
@@ -25,3 +28,29 @@ int main(){
 }
 
 //Write definition of randData(), showData() and findColSum()
+void randData(double *data,int N,int M) {
+	
+	return;
+}
+
+void findColSum(const double *data,double *output,int N,int M) {
+	
+	for (int i=0; i<N; i++) {		
+		for (int j=0; j<M; j++) {
+			*(output+j) += *(data + i*M+j);						
+		}   		
+	}
+	return;
+}
+
+void showData(double *data,int N,int M) {
+	
+	for (int i=0; i<N; i++) {		
+		for (int j=0; j<M; j++) {
+			cout << setw(5) << *(data+ i*M+j) << " ";				 
+		}   
+		cout << '\n';
+	}
+	
+	return;
+}
